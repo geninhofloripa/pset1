b95ba0f2d9019b3a5adb8d61e833832aaeb5841e
geninhofloripa <geninhofloripa@hotmail.com>
2016-05-21 20:21:44 -0300

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Eugenio Streliaev-Filho
+ * geninhofloripa@hotmail.com
+ * 
+ * hello.c
+ * 
+ *  Famous example program from the book K&R that just prints out the text
+ *  "hello, world" to the terminal, as an illustration of a minimal working
+ *  C program. Numerous texts since then have followed that convention for 
+ *  introducing a programming language.
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("hello, world\n");
+}
